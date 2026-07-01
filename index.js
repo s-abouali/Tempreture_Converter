ad5db9bfb25d6bab36357c3867bceaae50574019
Sarah Abouali <sarahabouali7@gmail.com>
2026-07-01 19:55:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const textbox = document.getElementById("textbox");
+const tofah = document.getElementById("tofah");
+const tocel = document.getElementById("tocel");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+
+    if(tofah.checked){
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(tocel.checked){
+        temp = Number(textbox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit"; 
+    }
+}
